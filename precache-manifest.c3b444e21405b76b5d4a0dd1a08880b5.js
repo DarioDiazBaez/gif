8f8ebdb8aaafa2fcab0fe6a821edc356bce512e5
Dario <dariodiazbaez@gmail.com>
2022-03-19 13:25:55 -0300

    Updates

---
+++ precache-manifest.c3b444e21405b76b5d4a0dd1a08880b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ea2859f272e752ff75ec0c470534c8",
+    "url": "/gif/index.html"
+  },
+  {
+    "revision": "cf7d9a9437ba12892fad",
+    "url": "/gif/static/css/main.10ba0c12.chunk.css"
+  },
+  {
+    "revision": "3437d601e63166c5fcd7",
+    "url": "/gif/static/js/2.c215d3c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gif/static/js/2.c215d3c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf7d9a9437ba12892fad",
+    "url": "/gif/static/js/main.2ac3d8ed.chunk.js"
+  },
+  {
+    "revision": "f166096f798da94251be",
+    "url": "/gif/static/js/runtime-main.35efd684.js"
+  }
+]);
